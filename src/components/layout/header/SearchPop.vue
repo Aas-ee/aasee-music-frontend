7fd36e834b7b8ca61c26f1b8ec43eb4a02d0f0bd
Aasee <1600677504@qq.com>
2025-06-10 20:43:47 +0800

    feat: 初始化

---
+++ src/components/layout/header/SearchPop.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-popover popper-style="max-width:auto;padding:0;" v-model:visible="showSearchView" width="250px">
+    <template #reference>
+      <ElInput placeholder="搜索音乐、MV、歌单"
+               :prefix-icon="Search"
+               clearable
+               />
+    </template>
+  </el-popover>
+</template>
+
+<!-- <script setup lang="ts">
+import {Search} from '@icon-park/vue-next'
+import {storeToRefs} from "pinia";
+import {useSearchStore} from "@/stores/search";
+import {onMounted, ref} from "vue";
+import type {SearchHotDetail} from "@/models/search";
+import {useSearchHotDetail} from "@/utils/api";
+
+import {debounce} from 'lodash'
+import SearchSuggest from "@/components/layout/header/SearchSuggest.vue";
+
+const {showSearchView, searchKeyword, showHot} = storeToRefs(useSearchStore())
+const {suggest} = useSearchStore()
+
+const hotClick = (text: string) => {
+  searchKeyword.value = text
+  suggest()
+  showSearchView.value = true
+}
+
+const searchInput = debounce((value: string | number) => suggest(), 500, {'maxWait': 1000})
+
+const searchHot = ref<SearchHotDetail[]>([])
+onMounted(async () => {
+  searchHot.value = await useSearchHotDetail()
+})
+
+</script>
+<style lang="scss">
+
+</style> -->
